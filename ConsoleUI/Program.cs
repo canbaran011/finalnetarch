454c1b36feaf31553844e4421ff7cfac5ae58d3d
Can Baran Gündüz <canbaran011@gmail.com>
2021-02-18 15:42:48 +0300

    first commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        // SOLID
+        // OpenClosed Principle -- IoC container
+        // DTO Data Transformation Object
+        static void Main(string[] args)
+        {
+            ProductTest();
+            //CategoryTest();
+
+
+             Console.WriteLine("Hello World!");
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine("Product Name : "+product.ProductName);
+                Console.WriteLine("Category Name: " + product.CategoryName);
+                Console.WriteLine("---------------------------------------");
+            }
+        }
+    }
+}
